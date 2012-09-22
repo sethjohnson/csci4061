6287c197742419f2bd8e60f76709d5a6302bcddf
Seth Johnson <seth@appfirst.com>
2012-09-22 18:25:38 -0500

    Handled command-line input

--- assignment01/main.c
+++ assignment01/main.c
@@ -6,10 +6,29 @@
 //
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+#define MAX_FILENAME_SIZE 200
+
+enum  {
+	EXIT_STATUS_BAD_INPUT
+	};
 int main(int argc, const char * argv[])
 {
-
-    return 0;
+	char input_file_name[MAX_FILENAME_SIZE];
+	
+	if (argc == 2)
+	{
+		strcpy(input_file_name, argv[1]);
+	}
+	else
+	{
+		printf("usage: graphexec inputfile\n");
+		exit(EXIT_STATUS_BAD_INPUT);
+	}
+	
+	
+	return 0;
 }
 
