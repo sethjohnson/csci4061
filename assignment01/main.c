91c7545ed516c7cd54fc68cc8852dcb7abca6cae
Seth Johnson <seth@appfirst.com>
2012-09-22 18:13:30 -0500

    Cleaned up main.c template

--- assignment01/main.c
+++ assignment01/main.c
@@ -1,7 +1,15 @@
+//
+//  main.c
+//  assignment01
+//
+//  Created by Seth Johnson and Michael Walton
+//
+
 #include <stdio.h>
 
-int main(int argc, char * argv[])
+int main(int argc, const char * argv[])
 {
 
-    return 0;	
+    return 0;
 }
+
