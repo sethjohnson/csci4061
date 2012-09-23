0abe5e96c92e4ae2c9edfc750e3d5bc6165b9458
Seth Johnson <seth@appfirst.com>
2012-09-23 14:43:29 -0500

    Added some line-parsing code

--- assignment01/main.c
+++ assignment01/main.c
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
 #include <stdlib.h>
 
 #define MAX_FILENAME_SIZE 200
@@ -14,21 +15,72 @@
 enum  {
 	EXIT_STATUS_BAD_INPUT
 	};
+
+
+void parse_line(const char * line, const char seperator, char ** destination)
+{
+	char* end = strchr(line, seperator);
+	long len;
+	if (!end)
+	{
+		len = strlen(line);
+		if(strpbrk(line, "\n\r"))
+			len--; // remove race of line break
+
+		*destination = malloc(sizeof(char)*(len+1));
+		assert(*destination);
+		memcpy(*destination, line, len);
+		(*destination)[len] = '\0';
+	}
+	else
+	{
+		len = end-line;
+		*destination = malloc(sizeof(char)*(len+1));
+		assert(*destination);
+
+		memcpy(*destination, line, len);
+		(*destination)[len] = '\0';
+		parse_line(end+1, seperator, destination+1);
+
+	}
+	
+}
+
 int main(int argc, const char * argv[])
 {
 	char input_file_name[MAX_FILENAME_SIZE];
 	
 	if (argc == 2)
 	{
+		// If there are 2 arguments, the second one will be the input file
 		strcpy(input_file_name, argv[1]);
 	}
 	else
 	{
+		// If there are not 2 arguments, print usage message and exit
 		printf("usage: graphexec inputfile\n");
 		exit(EXIT_STATUS_BAD_INPUT);
 	}
 	
+	FILE* input_file = fopen(input_file_name, "r");
+	if (!input_file) {
+		printf("The file %s does not exist or could not be opened.\n",input_file_name);
+	}
+
+	char line[MAX_LINE_SIZE];
 	
+	while (fgets(line, MAX_LINE_SIZE, input_file)) {
+
+		char** parameters = calloc(MAX_LINE_COUNT, sizeof(char*));
+
+		parse_line(line, ':', parameters);
+		
+		printf("Command: %s\n",parameters[0]);
+		printf("Children: %s\n",parameters[1]);
+		printf("Input stream: %s\n",parameters[2]);
+		printf("Output stream: %s\n",parameters[3]);
+
+	}
 	return 0;
 }
 
